22072facdb123ddaa6ffdaae9c70dbe11b217a62
DanielAlk <daniodfad@yahoo.com.ar>
2015-12-09 05:20:51 -0300

    layout home working

---
+++ app/assets/javascripts/utils.js
@@ -0,0 +1,20 @@
+var Utils = {};
+
+Utils.toggler = function() {
+	var show = function($target) {
+		$target.stop(true,true).addClass('active').fadeIn();
+	};
+	var hide = function($target) {
+		$target.stop(true,true).removeClass('active').fadeOut();
+	};
+	var toggle = function($target) {
+		if ($target.hasClass('active')) hide($target);
+		else show($target);
+	};
+	var handler = function(e) {
+		e.preventDefault();
+		var $target = $($(this).data('toggle'));
+		toggle($target);
+	};
+	$('[data-toggle]').click(handler);
+};
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+	def head_allow_robots?
+		true
+	end
+
+	def head_title
+		'Freixenet'
+	end
+
+	def head_description
+		''
+	end
+
+	def head_og_image
+		''
+	end
+
+	def navigation_background
+		'bg-beach-party'
+	end
+
+	def navigation_class(action, match_to = :action)
+		'active' if action == params[match_to]
+	end
 end
