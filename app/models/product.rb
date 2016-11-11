747301e66dde990713de0f918c7446aa3e1120f9
DanielAlk <daniodfad@yahoo.com.ar>
2016-11-11 18:23:57 -0300

    sanitize tinymce for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,10 @@ class Product < ActiveRecord::Base
 										convert_options: { large: "-quality 75 -strip" }
 	validates_attachment_content_type :file, content_type: /\Aapplication\/pdf\Z/
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+	def text=(argument)
+		sanitizer = Rails::Html::WhiteListSanitizer.new
+  	sanitized = sanitizer.sanitize(argument, tags: %w(strong em br a), attributes: %w(href))
+  	write_attribute(:text, sanitized)
+	end
 end
